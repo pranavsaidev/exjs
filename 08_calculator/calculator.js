4d0ba723755f75cf61a07444dbc3e7f196e00f58
REDROUTHUPRANAV\Pranav <redrouthu@vyaparapp.in>
2023-06-23 16:48:44 +0530

    complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return +a + +b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return +a - +b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let ans=1;
+	for(let i = 1; i <= a; i++) {
+		ans *= i;
+	}
+	return ans;
 };
 
 // Do not edit below this line
